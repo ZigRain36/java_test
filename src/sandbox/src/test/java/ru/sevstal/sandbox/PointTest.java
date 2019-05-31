7cb90a99eb32c009a2820713dcb52e42a3f88801
ZigRain36 <50735148+ZigRain36@users.noreply.github.com>
2019-05-31 11:02:24 +0300

    добавлен класс по модификации и удалению контакта. задействован switchTo().alert().accept();.выполнено задание 6

---
+++ src/sandbox/src/test/java/ru/sevstal/sandbox/PointTest.java
@@ -0,0 +1,15 @@
+package ru.sevstal.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
+public class PointTest {
+    @Test
+    public void testPoint() {
+        Point p1 = new Point(8, 4);
+        Point p2 = new Point(16, 4);
+        Assert.assertEquals(p1.distance(p2), 8.0);
+   }
+
+}
