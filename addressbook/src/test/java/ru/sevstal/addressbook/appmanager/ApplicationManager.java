0fc2341ead110c3ad259f406749514119733e960
ZigRain36 <50735148+ZigRain36@users.noreply.github.com>
2019-06-03 12:29:53 +0300

    добавлена возможность запускать тесты с разных брайзеров

--- addressbook/src/test/java/ru/sevstal/addressbook/appmanager/ApplicationManager.java
+++ addressbook/src/test/java/ru/sevstal/addressbook/appmanager/ApplicationManager.java
@@ -3,8 +3,12 @@ package ru.sevstal.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,10 +19,28 @@ public class ApplicationManager {
     private NavigationHepler navigationHepler;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        System.setProperty("webdriver.gecko.driver", "C:\\Books\\geckodriver.exe");
-        wd = new FirefoxDriver();
+        if (browser == BrowserType.FIREFOX){
+            System.setProperty("webdriver.gecko.driver", "C:\\Books\\geckodriver.exe");
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.CHROME) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Books\\chromedriver.exe");
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.IE) {
+            System.setProperty("webdriver.ie.driver", "C:\\Books\\IEDriverServer.exe");
+            DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
+            ieCapabilities.setCapability(
+                    InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
+                    true
+            );
+            wd = new InternetExplorerDriver(ieCapabilities);
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
--- addressbook/src/test/java/ru/sevstal/addressbook/tests/TestBase.java
+++ addressbook/src/test/java/ru/sevstal/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.sevstal.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.sevstal.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
     @BeforeMethod
     public void setUp() throws Exception {
         app.init();
