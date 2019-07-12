29420cd145a94e65a61c99760917c589cbec7047
ZigRain36 <50735148+ZigRain36@users.noreply.github.com>
2019-07-12 11:14:55 +0300

    не видит файл hibernate.cfg.xml

--- addressbook/src/test/java/ru/sevstal/addressbook/tests/HBConnectionTest.java
+++ addressbook/src/test/java/ru/sevstal/addressbook/tests/HBConnectionTest.java
@@ -14,32 +14,35 @@ import java.util.List;
 
 public class HBConnectionTest {
 
+
     private SessionFactory sessionFactory;
 
     @BeforeClass
     protected void setUp() throws Exception {
+
         // A SessionFactory is set up once for an application!
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure() // configures settings from hibernate.cfg.xml
                 .build();
-
         try {
-
-            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            sessionFactory = new MetadataSources(registry)
+                    .buildMetadata()
+                    .buildSessionFactory();
         } catch (Exception e) {
-            e.printStackTrace();
             // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
             // so destroy it manually.
+            e.printStackTrace();
             StandardServiceRegistryBuilder.destroy(registry);
         }
     }
 
     @Test
-    public void testHBConnection() {
+    public void hbConnectionTest() {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        List<GroupData> result = session.createQuery("from ru.pft.addressbook.model.GroupData").list();
-        for (GroupData group : result) {
+        List result = session.createQuery("from ru.sevstal.addressbook.model.GroupData")
+                .list();
+        for (GroupData group : (List<GroupData>) result) {
             System.out.println(group);
         }
         session.getTransaction().commit();
