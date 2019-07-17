f23db23a50dd2737a0f5f01b604ffecf8f4cc37a
ZigRain36 <50735148+ZigRain36@users.noreply.github.com>
2019-07-17 14:04:15 +0300

    выполнено задание #17.добавлен новый модуль для тестов Mantis

---
+++ mantis-tests/src/test/java/ru/sevstal/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,68 @@
+package ru.sevstal.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    private final Properties properties;
+    public WebDriver wd;
+
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(
+                String.format("src/test/resources/%s.properties", target))));
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            System.setProperty("webdriver.gecko.driver", "C:\\Books\\geckodriver.exe");
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Books\\chromedriver.exe");
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            System.setProperty("webdriver.ie.driver", "C:\\Books\\IEDriverServer.exe");
+            DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
+            ieCapabilities.setCapability(
+                    InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+            wd = new InternetExplorerDriver(ieCapabilities);
+        }
+
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
+
+
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+    boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+
+}
---
+++ mantis-tests/src/test/java/ru/sevstal/mantis/tests/TestBase.java
@@ -0,0 +1,24 @@
+package ru.sevstal.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.sevstal.mantis.appmanager.ApplicationManager;
+
+public class TestBase {
+
+    protected static final ApplicationManager
+            app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+    @BeforeSuite
+    void setUp() throws Exception {
+        app.init();
+
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+
+}
