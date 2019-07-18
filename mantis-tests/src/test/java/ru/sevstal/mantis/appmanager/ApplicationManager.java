83d3cf7281e85485dacfae8201f88a64f6b9b617
ZigRain36 <50735148+ZigRain36@users.noreply.github.com>
2019-07-18 09:38:35 +0300

    выполнено задание #17.добавлен новый модуль для тестов Mantis

--- mantis-tests/src/test/java/ru/sevstal/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/sevstal/mantis/appmanager/ApplicationManager.java
@@ -17,9 +17,10 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     private final Properties properties;
-    public WebDriver wd;
+    private WebDriver wd;
 
     private String browser;
+    private RegistrationHelper registrtationHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -30,29 +31,12 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(
                 String.format("src/test/resources/%s.properties", target))));
-
-        if (browser.equals(BrowserType.FIREFOX)) {
-            System.setProperty("webdriver.gecko.driver", "C:\\Books\\geckodriver.exe");
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {
-            System.setProperty("webdriver.chrome.driver", "C:\\Books\\chromedriver.exe");
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            System.setProperty("webdriver.ie.driver", "C:\\Books\\IEDriverServer.exe");
-            DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
-            ieCapabilities.setCapability(
-                    InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-            wd = new InternetExplorerDriver(ieCapabilities);
-        }
-
-        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        wd.get(properties.getProperty("web.baseUrl"));
-
-
-    }
+             }
 
     public void stop() {
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
     }
 
     boolean isElementPresent(By by) {
@@ -63,6 +47,40 @@ public class ApplicationManager {
             return false;
         }
     }
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
 
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrtationHelper == null) {
+            registrtationHelper =  new RegistrationHelper(this);
+        }
+        return registrtationHelper;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                System.setProperty("webdriver.gecko.driver", "C:\\Books\\geckodriver.exe");
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                System.setProperty("webdriver.chrome.driver", "C:\\Books\\chromedriver.exe");
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                System.setProperty("webdriver.ie.driver", "C:\\Books\\IEDriverServer.exe");
+                DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
+                ieCapabilities.setCapability(
+                        InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+                wd = new InternetExplorerDriver(ieCapabilities);
+            }
+            wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
+    }
 
 }
---
+++ mantis-tests/src/test/java/ru/sevstal/mantis/tests/RegistrationTests.java
@@ -0,0 +1,15 @@
+package ru.sevstal.mantis.tests;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase {
+
+
+    @Test
+
+    public void testRegistration() {
+        app.registration().start("user", "user1@localhost.localdomain");
+
+    }
+
+}
